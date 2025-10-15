661352328e956f022e862085178ac2d3ac75e97e
Suporte <suportemusicdelivery1@gmail.com>
2025-10-15 09:46:42 -0300

    Add basename to BrowserRouter for correct routing on GitHub Pages.

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+  <BrowserRouter basename="/cadastrosmd-automation-web">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/auth" element={<Auth />} />
